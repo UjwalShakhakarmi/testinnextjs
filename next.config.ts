3470f6d4cfd096eac0074c6d5c3b0b7450d420e3
UjwalShakhakarmi <ujwalshakha@gmail.com>
2025-10-28 11:55:56 +0545

    use of localstorage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BASEURL: "https://flutter.mydynamicerp.com/",
+    BASEURL: "https://kalikaschoolbtl.mydynamicerp.com/",
   },
 };
 
